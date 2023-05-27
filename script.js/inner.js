2dab6f6ed6be6d1aad53ff5e235a2b845b620460
ZulmaM <zulmacris1995@gmail.com>
2023-05-27 08:07:41 -0300

    footer terminado y enlazado mediante id

--- script.js/inner.js
+++ script.js/inner.js
@@ -53,9 +53,94 @@ element.innerHTML = '<header id="header" class="headerbienvenida">'+
   '</div>'+
   '</div>'+
   '</nav>'+
-  '</section>';                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-
-    
+  '</section>';  
   
-
-
+  var element = document.getElementById("footer");
+  element.innerHTML = '<footer id="footer" class="footerclass">'+
+  '<section id="scrollpycontacto">'+
+  '<div class="container">'+
+  '<div class="row align-items-start">'+
+  '<div class="col">'+
+  '<div class="contacto">'+
+  '<h4 class="titcontacto">'+
+  '<nav class="navbar navbar-light bg-light">'+
+  '<div class="d-flex justify-content-between align-items-center">'+
+  '<div class="container-fluid">'+
+  '<ul class="nav nav-pills">'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="index.html">'+
+  '<h6 class="titulosnavfooter">Inicio</h6>'+
+  '</a>'+
+  '</li>'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="quienessomos.html">'+
+  '<h6 class="titulosnavfooter">¿Quienes somos?</h6>'+
+  '</a>'+
+  '</li>'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="huerta.html">'+
+  '<h6 class="titulosnavfooter">Huerta</h6>'+
+  '</a>'+
+  '</li>'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="tienda.html">'+
+  '<h6 class="titulosnavfooter">Tienda</h6>'+
+  '</a>'+
+  '</li>'+
+  '</ul>'+
+  '</div>'+
+  '<div class="icon-containerfooter">'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="#scrollpycontacto">'+
+  '<a href="https://www.instagram.com/huerta_zuculenta/" target="_blank">'+
+  '<img src="../assets/logotipo-de-instagram.png" alt="Cartel redes">'+
+  '</a>'+
+  '</a>'+
+  '</li>'+
+  '</div>'+
+  '<div class="icon-containerfooter">'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="#scrollpycontacto">'+
+  '<a href="https://api.whatsapp.com/send?phone=1153749972" target="_blank">'+
+  '<img src="../assets/whatsapp.png" alt="Cartel redes">'+
+  '</a>'+
+  '</a>'+
+  '</li>'+
+  '</div>'+
+  '<div class="icon-containerfooter">'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="#scrollpycontacto">'+
+  '<a href="" target="_blank">'+
+  '<img src="../assets/facebook.png" alt="Cartel redes">'+
+  '</a>'+
+  '</a>'+
+  '</li>'+
+  '</div>'+
+  '<div class="icon-containerfooter">'+
+  '<li class="nav-item">'+
+  '<a class="nav-link" href="tienda.html">'+
+  '<a href="tienda.html" target="_blank">'+
+  '<img src="../assets/carretilla.png" alt="Cartel redes">'+
+  '</a>'+
+  '</a>'+
+  '</li>'+
+  '</div>'+
+  '</div>'+
+  '</nav>'+
+  '</h4>'+
+  '</div>'+
+  '</div>'+
+  '</div>'+
+  '</div>'+
+  '</section>'+
+  '<div class="row align-items-center">'+
+  '<div class="col">'+
+  '<img class="imgfooter" src="../assets/1CartelRedes.jpg" alt="">'+
+  '</div>'+
+  '<div class="col">'+
+  '<p class="parrafofooter">Derechos de autor© 2023. Todos los derechos reservados.</p>'+
+  '<p class="parrafofooter">Propiedad de huerta zuculenta</p>'+
+  '</div>'+
+  '</div>'+
+  '</footer>';
+  
