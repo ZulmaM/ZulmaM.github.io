c90c8ab0e4c63ac9b7597cbc316db31c120b3256
ZulmaM <zulmacris1995@gmail.com>
2023-06-08 01:09:24 -0300

    se modifica menu hamburgueza por barra de navegacion, se agrega funcion java a la tienda

--- script.js/const.js
+++ script.js/const.js
@@ -1,6 +1,69 @@
-const menuBtn = document.querySelector(".menu-btn");
-const header = document.querySelector("#header");
+// Variables
+const carrito = [];
+const items = document.getElementById('items');
+const carritoElemento = document.getElementById('carrito');
+const total = document.getElementById('total');
+const totalCarritoElemento = document.getElementById('total-carrito');
+const botonVaciar = document.getElementById('boton-vaciar');
 
-menuBtn.addEventListener("click", () => {
-  header.classList.toggle("open");
+// Función para agregar un producto al carrito
+function agregarProducto(evento) {
+  const boton = evento.target;
+  const item = boton.closest('.card');
+  const itemId = item.querySelector('.agregar-carrito').dataset.id;
+  const itemNombre = item.querySelector('.card-title').textContent;
+  const itemPrecio = item.querySelector('.agregar-carrito').dataset.precio;
+
+  const producto = {
+    id: itemId,
+    nombre: itemNombre,
+    precio: itemPrecio
+  };
+
+  carrito.push(producto);
+  calcularTotal();
+  actualizarCarritoHTML();
+}
+
+// Función para calcular el total del carrito
+function calcularTotal() {
+  let totalCarrito = 0;
+  for (let i = 0; i < carrito.length; i++) {
+    totalCarrito += parseFloat(carrito[i].precio);
+  }
+  total.textContent = totalCarrito.toFixed(2);
+  totalCarritoElemento.textContent = "(" + carrito.length + ")";
+}
+
+// Función para actualizar el carrito en el HTML
+function actualizarCarritoHTML() {
+  carritoElemento.innerHTML = '';
+  for (let i = 0; i < carrito.length; i++) {
+    const productoElemento = document.createElement('li');
+    productoElemento.classList.add('list-group-item');
+    productoElemento.textContent = `${carrito[i].nombre} - $${carrito[i].precio}`;
+    carritoElemento.appendChild(productoElemento);
+  }
+}
+
+// Función para vaciar el carrito
+function vaciarCarrito() {
+  carrito.splice(0, carrito.length);
+  actualizarCarritoHTML();
+  calcularTotal();
+}
+
+// Evento para agregar producto al hacer clic en el botón "Agregar al carrito"
+items.addEventListener('click', function(evento) {
+  if (evento.target.classList.contains('agregar-carrito')) {
+    agregarProducto(evento);
+  }
 });
+
+// Evento para vaciar el carrito al hacer clic en el botón "Vaciar"
+botonVaciar.addEventListener('click', vaciarCarrito);
+
+// Llamar a la función para actualizar el carrito inicialmente
+calcularTotal();
+actualizarCarritoHTML();
+
--- script.js/inner.js
+++ script.js/inner.js
@@ -1,103 +1,113 @@
 /* llamamos al header mediante id "header"*/
-var element = document.getElementById("header1");
-element.innerHTML = ' <section id="header1">'+
-'<header id="header" class="headerbienvenida">'+
-'<button class="menu-btn"></button>'+
-'<div class="backdrop"></div>'+
-'<div class="menu">'+
-'<nav>'+
-'<a href="index.html"  style="animation-delay: 0.2s;">Inicio</a>'+
-'<a href="quienessomos.html"  style="animation-delay: 0.3s;">Quienes somos?</a>'+
-'<a href="huerta.html"  style="animation-delay: 0.4s;">Huerta</a>'+
-'<a href="tienda.html"  style="animation-delay: 0.5s;">Tienda</a>'+
-'<a href="#scrollpycontacto" style="animation-delay: 0.6s;">Contacto</a>'+
-'</nav>'+
-'</div>'+
-'<div  class="bienvenida">'+
+var element = document.getElementById("header");
+element.innerHTML = '<header id="header" class="headerbienvenida">'+
+'<div class="bienvenida">'+
 '<h1>'+
 '<span class="resaltado">Bienvenidos a Huerta Zuculenta</span>'+
 '</h1>'+
 '</div>'+
-'</header>'+
-'</section>';
+'</header>';
   
+/* llamamos al header mediante id "navbar"*/
+var element = document.getElementById("navbar");
+element.innerHTML = '<section id="navbar" class="barranav">'+      
+'<nav class="navbar navbar-expand-lg ">'+
+'  <div class="container-fluid">'+
+'    <a class="navbar-brand" href="index.html">Inicio</a>'+
+'    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+'      <span class="navbar-toggler-icon"></span>'+
+'    </button>'+
+'    <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+'      <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
+'        <li class="nav-item">'+
+'          <a class="nav-link active" aria-current="page" href="quienessomos.html">¿Quienes somos?</a>'+
+'        </li>'+
+'        <li class="nav-item">'+
+'          <a class="nav-link active" href="tienda.html">Tienda</a>'+
+'        </li>'+
+'        <li class="nav-item dropdown">'+
+'          <a class="nav-link dropdown-toggle active" href="huerta.html" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Huerta</a>'+
+'          <ul class="dropdown-menu">'+
+'            <li><a class="dropdown-item" href="#">Tipos de huertas</a></li>'+
+'            <li><a class="dropdown-item" href="#">Calendario de siembra</a></li>'+
+'            <li><a class="dropdown-item" href="#">aromaticas</a></li>'+
+'          </ul>'+
+'        </li>'+
+'      </ul>'+
+'      <div class="d-flex search-form">'+
+'        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">'+
+'        <button class="btn btn-outline-success" type="submit">'+
+'          <i class="fas fa-search"></i>'+
+'        </button>'+
+'      </div>'+
+'    </div>'+
+'  </div>'+
+'</nav>'+
+'</section>';
+
+
   /* llamamos al header mediante id "footer"*/
   var element = document.getElementById("footer");
-  element.innerHTML = '<footer id="footer" class="footerclass">'+
-  '<section id="scrollpycontacto">'+
-  '<div class="d-flex justify-content-between align-items-center">'+
-  '<div class="container-fluid">'+
-  '<ul class="icon-containerfooter">'+
-  '<li class="listfooter">'+
-  '<a href="index.html">'+
-  '<h6 class="titulosnavfooter">Inicio</h6>'+
-  '</a>'+
-  '</li>'+
-  '<li class="listfooter">'+
-  '<a href="quienessomos.html">'+
-  '<h6 class="titulosnavfooter">¿Quienes somos?</h6>'+
-  '</a>'+
-  '</li>'+
-  '<li class="listfooter">'+
-  '<a href="huerta.html">'+
-  '<h6 class="titulosnavfooter">Huerta</h6>'+
-  '</a>'+
-  '</li>'+
-  '<li class="listfooter">'+
-  '<a href="tienda.html">'+
-  '<h6 class="titulosnavfooter">Tienda</h6>'+
-  '</a>'+
-  '</li>'+
-  '</ul>'+
-  '</div>'+
-  '<div class="icon-containerfooter desplazamiento">'+
-  '<li>'+
-  '<a href="#scrollpycontacto">'+
-  '<a href="https://www.instagram.com/huerta_zuculenta/" target="_blank">'+
-  '<img src="../assets/logotipo-de-instagram.png" alt="logo instagram">'+
-  '</a>'+
-  '</a>'+
-  '</li>'+
-  '</div>'+
-  '<div class="icon-containerfooter desplazamiento">'+
-  '<li>'+
-  '<a href="#scrollpycontacto">'+
-  '<a href="https://api.whatsapp.com/send?phone=1153749972" target="_blank">'+
-  '<img src="../assets/whatsapp.png" alt="logo whatsapp">'+
-  '</a>'+
-  '</a>'+
-  '</li>'+
-  '</div>'+
-  '<div class="icon-containerfooter desplazamiento">'+
-  '<li>'+
-  '<a href="#scrollpycontacto">'+
-  '<a href="https://www.facebook.com/" target="_blank">'+
-  '<img src="../assets/facebook.png" alt="logo facebook">'+
-  '</a>'+
-  '</a>'+
-  '</li>'+
-  '</div>'+
-  '<div class="icon-containerfooter desplazamiento">'+
-  '<li>'+
-  '<a href="tienda.html">'+
-  '<a href="/tienda.html" target="_blank">'+
-  '<img src="../assets/carretilla.png" alt="logo Carrito de compras">'+
-  '</a>'+
-  '</a>'+
-  '</li>'+
-  '</div>'+
-  '</div>'+
-  '</section>'+
-  '<div class="row align-items-center">'+
-  '<div class="col">'+
-  '<img class="imgfooter" src="../assets/1CartelRedes.jpg" alt="logo huerta zuculenta">'+
-  '</div>'+
-  '<div class="col">'+
-  '<p class="parrafofooter">Proyecto final B.A. Multiplica 2.0. Clase 59</p>'+
-  '<p class="parrafofooter">Realizado por Marcos Zulma</p>'+
-  '</div>'+
-  '</div>'+
+  element.innerHTML = '<footer id="footer" class="footerclass">' +
+    '<section id="scrollpycontacto">' +
+      '<div class="d-flex justify-content-between align-items-center">' +
+        '<div class="container-fluid">' +
+          '<ul class="icon-containerfooter">' +
+            '<li class="listfooter">' +
+              '<a href="index.html">' +
+                '<h6 class="titulosnavfooter">Inicio</h6>' +
+              '</a>' +
+            '</li>' +
+            '<li class="listfooter">' +
+              '<a href="quienessomos.html">' +
+                '<h6 class="titulosnavfooter">¿Quienes somos?</h6>' +
+              '</a>' +
+            '</li>' +
+            '<li class="listfooter">' +
+              '<a href="tienda.html">' +
+                '<h6 class="titulosnavfooter">Tienda</h6>' +
+              '</a>' +
+            '</li>' +
+            '<li class="listfooter">' +
+              '<a href="huerta.html">' +
+                '<h6 class="titulosnavfooter">Huerta</h6>' +
+              '</a>' +
+            '</li>' +
+          '</ul>' +
+        '</div>' +
+        '<div class="icon-containerfooter desplazamiento">' +
+          '<a href="https://www.instagram.com/huerta_zuculenta/" target="_blank">' +
+            '<img src="../assets/logotipo-de-instagram.png" alt="logo instagram">' +
+          '</a>' +
+        '</div>' +
+        '<div class="icon-containerfooter desplazamiento">' +
+          '<a href="https://api.whatsapp.com/send?phone=1153749972" target="_blank">' +
+            '<img src="../assets/whatsapp.png" alt="logo whatsapp">' +
+          '</a>' +
+        '</div>' +
+        '<div class="icon-containerfooter desplazamiento">' +
+          '<a href="https://www.facebook.com/" target="_blank">' +
+            '<img src="../assets/facebook.png" alt="logo facebook">' +
+          '</a>' +
+        '</div>' +
+        '<div class="icon-containerfooter desplazamiento">' +
+          '<a href="tienda.html" target="_blank">' +
+            '<img src="../assets/carretilla.png" alt="logo Carrito de compras">' +
+          '</a>' +
+        '</div>' +
+      '</div>' +
+    '</section>' +
+    '<div class="row align-items-center">' +
+      '<div class="col">' +
+        '<img class="imgfooter" src="../assets/1CartelRedes.jpg" alt="logo huerta zuculenta">' +
+      '</div>' +
+      '<div class="col">' +
+        '<p class="parrafofooter">Proyecto final B.A. Multiplica 2.0. Clase 59</p>' +
+        '<p class="parrafofooter">Realizado por Marcos Zulma</p>' +
+      '</div>' +
+    '</div>' +
   '</footer>';
+  
   
 
 
