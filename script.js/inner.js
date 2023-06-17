adf749764cd37b1206fd2c33ffeae07cbf0d86ad
ZulmaM <zulmacris1995@gmail.com>
2023-06-16 21:57:58 -0300

    se corrige archivo inner e id navbar

--- script.js/inner.js
+++ script.js/inner.js
@@ -16,7 +16,7 @@ var navbarElement = document.getElementById("navbar1");
 navbarElement.innerHTML = '<section id="navbar1" class="barranav">' +
     '<nav class="navbar navbar-expand-lg ">' +
     '  <div class="container-fluid">' +
-    '    <a class="navbar-brand" href="index.html">Inicio</a>' +
+    '    <a class="navbar-brand" href="/index.html">Inicio</a>' +
     '    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
     '      <span class="navbar-toggler-icon"></span>' +
     '    </button>' +
