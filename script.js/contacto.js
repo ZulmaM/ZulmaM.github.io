2af67d2b359c3841798bd5ed6f2f36dc6e5b52df
ZulmaM <zulmacris1995@gmail.com>
2023-06-17 00:06:52 -0300

    se corrige los archivos css

--- script.js/contacto.js
+++ script.js/contacto.js
@@ -1,33 +1,40 @@
-function validar_formulario() {
+function validar_formulario(event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+  
     var todo_correcto = true;
     var nombre = document.getElementById('nombre').value;
     var mail = document.getElementById('mail').value;
     var texto = document.getElementById('mensaje').value;
     var checkbox = document.getElementById('check');
-
+  
     if (nombre.length < 2) {
-        todo_correcto = false;
+      todo_correcto = false;
     }
-
+  
     var expresion = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+  
     if (!expresion.test(mail)) {
-        todo_correcto = false;
+      todo_correcto = false;
     }
-
+  
     if (texto === '') {
-        alert("El textarea no puede estar vacío");
-        todo_correcto = false;
+      alert("El textarea no puede estar vacío");
+      todo_correcto = false;
     }
-
+  
     if (!checkbox.checked) {
-        todo_correcto = false;
+      todo_correcto = false;
     }
-
-    if (!todo_correcto) {
-        alert('Algunos campos no están correctos, revisa los que te quedan');
+  
+    if (todo_correcto) {
+      // seccion notificación de agradecimiento
+      alert('¡Gracias por contactarnos!');
+      form.reset(); // Reinicia el formulario
+    } else {
+      alert('Algunos campos no están correctos, revisa los que te quedan');
     }
-}
-
-var form = document.querySelector('form');
-form.addEventListener('submit', validar_formulario);
+  }
+  
+  var form = document.querySelector('form');
+  form.addEventListener('submit', validar_formulario);
+  
--- script.js/inner.js
+++ script.js/inner.js
@@ -1,112 +1,83 @@
 
 /* llamamos al header mediante id "header" */
 var headerElement = document.getElementById("header");
-headerElement.innerHTML = '<header id="header" class="headerbienvenida">' +
-    '<a href="index.html" class="linkheader">' +
-    '  <div class="bienvenida">' +
-    '    <h1>' +
-    '      <span class="resaltado">Bienvenidos a Huerta Zuculenta</span>' +
-    '    </h1>' +
-    '  </div>' +
-    '</a>' +
+headerElement.innerHTML = '<header id="header" class="headerbienvenida">'+
+    '<a href="index.html" class="linkheader">'+
+    '  <div class="bienvenida">'+
+    '    <h1>'+
+    '      <span class="resaltado">Bienvenidos a Huerta Zuculenta</span>'+
+    '    </h1>'+
+    '  </div>'+
+    '</a>'+
     '</header>';
 
 /* llamamos al navbar mediante id "navbar1" */
 var navbarElement = document.getElementById("navbar1");
-navbarElement.innerHTML = '<section id="navbar1" class="barranav">' +
-    '<nav class="navbar navbar-expand-lg ">' +
-    '  <div class="container-fluid">' +
-    '    <a class="navbar-brand" href="/index.html">Inicio</a>' +
-    '    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
-    '      <span class="navbar-toggler-icon"></span>' +
-    '    </button>' +
-    '    <div class="collapse navbar-collapse" id="navbarSupportedContent">' +
-    '      <ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
-    '        <li class="nav-item">' +
-    '          <a class="nav-link active" aria-current="page" href="../pages/quienessomos.html">¿Quienes somos?</a>' +
-    '        </li>' +
-    '        <li class="nav-item">' +
-    '          <a class="nav-link active" href="../pages/tienda.html">Tienda</a>' +
-    '        </li>' +
-    '          <li class="nav-item">' +
-    '          <a class="nav-link active" href="../pages/contacto.html">Contacto</a>' +
-    '          </li>' +
-    '      </ul>' +
-    '      <div class="d-flex search-form">' +
-    '        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">' +
-    '        <button class="btn btn-outline-success" type="submit">' +
-    '          <i class="fas fa-search"></i>' +
-    '        </button>' +
-    '      </div>' +
-    '    </div>' +
-    '  </div>' +
-    '</nav>' +
+navbarElement.innerHTML = '<section id="navbar1" class="barranav">'+
+    '<nav class="navbar navbar-expand-lg ">'+
+    '  <div class="container-fluid">'+
+    '    <a class="navbar-brand" href="/index.html">Inicio</a>'+
+    '    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+    '      <span class="navbar-toggler-icon"></span>'+
+    '    </button>'+
+    '    <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+    '      <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
+    '        <li class="nav-item">'+
+    '          <a class="nav-link active" aria-current="page" href="../pages/quienessomos.html">¿Quienes somos?</a>'+
+    '        </li>'+
+    '        <li class="nav-item">'+
+    '          <a class="nav-link active" href="../pages/tienda.html">Tienda</a>'+
+    '        </li>'+
+    '          <li class="nav-item">'+
+    '          <a class="nav-link active" href="../pages/contacto.html">Contacto</a>'+
+    '          </li>'+
+    '      </ul>'+
+    '      <div class="d-flex search-form">'+
+    '        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">'+
+    '        <button class="btn btn-outline-success" type="submit">'+
+    '          <i class="fas fa-search"></i>'+
+    '        </button>'+
+    '      </div>'+
+    '    </div>'+
+    '  </div>'+
+    '</nav>'+
     '</section>';
 
 
-  /* llamamos al header mediante id "footer"*/
-  var element = document.getElementById("footer");
-  element.innerHTML = '<footer id="footer" class="footerclass">'+
-'    <section id="scrollpycontacto">'+
-'      <div class="d-flex justify-content-between align-items-center">'+
-'        <div class="container-fluid">'+
-'          <ul class="icon-containerfooter">'+
-'            <li class="listfooter">'+
-'              <a href="/index.html">'+
-'                <h6 class="titulosnavfooter">Inicio</h6>'+
-'              </a>'+
-'            </li>'+
-'            <li class="listfooter">'+
-'              <a href="quienessomos.html">'+
-'                <h6 class="titulosnavfooter">¿Quienes somos?</h6>'+
-'              </a>'+
-'            </li>'+
-'            <li class="listfooter">'+
-'              <a href="tienda.html">'+
-'                <h6 class="titulosnavfooter">Tienda</h6>'+
-'              </a>'+
-'            </li>'+
-'            <li class="listfooter">'+
-'              <a href="contacto.html">'+
-'                <h6 class="titulosnavfooter">Contacto</h6>'+
-'              </a>'+
-'            </li>'+
-'          </ul>'+
-'        </div>'+
-'        <div class="icon-containerfooter desplazamiento">'+
-'          <a href="https://www.instagram.com/huerta_zuculenta/" target="_blank">'+
-'            <img src="../media/logotipo-de-instagram.png" alt="logo instagram">'+
-'          </a>'+
-'        </div>'+
-'        <div class="icon-containerfooter desplazamiento">'+
-'          <a href="https://api.whatsapp.com/send?phone=1153749972" target="_blank">'+
-'            <img src="../media/whatsapp.png" alt="logo whatsapp">'+
-'          </a>'+
-'        </div>'+
-'        <div class="icon-containerfooter desplazamiento">'+
-'          <a href="https://www.facebook.com/" target="_blank">'+
-'            <img src="../media/facebook.png" alt="logo facebook">'+
-'          </a>'+
-'        </div>'+
-'        <div class="icon-containerfooter desplazamiento">'+
-'          <a href="tienda.html" target="_blank">'+
-'            <img src="../media/carretilla.png" alt="logo Carrito de compras">'+
-'          </a>'+
-'        </div>'+
-'      </div>'+
-'    </section>'+
-'    <div class="row align-items-center">'+
-'    <a href="tienda.html">'+
-'    <div class="col">'+
-'    <img class="imgfooter" src="../media/1CartelRedes.jpg" alt="logo huerta zuculenta">'+
-'    </div>'+
-'    </a>'+
-'      <div class="col">'+
-'        <p class="parrafofooter">Proyecto final B.A. Multiplica 2.0. Clase 59</p>'+
-'        <p class="parrafofooter">Realizado por Marcos Zulma</p>'+
-'      </div>'+
-'    </div>'+
-'  </footer>';
+/* llamamos al footer mediante id "footer"*/
+var element = document.getElementById("footer");
+element.innerHTML = '<section id="footer">'+
+  '<footer>'+
+  '<div class="container__footer">'+
+  '    <div class="box__footer">'+
+  '        <div class="logo">'+
+  '            <a href="https://www.instagram.com/huerta_zuculenta/" target="_blank"><img src="../media/CartelRedes.jpg" alt="cartelredes"></a>'+
+  '        </div>'+
+  '        <div class="pfooter">'+
+  '          <h4>¿Por que tener una huerta?</h4>'+
+  '          <p>El acto de sembrar, observar el crecimiento de las plantas y participar en el ciclo de la vida puede ayudar a reducir el estrés, fomentar la atención plena y brindar una sensación de paz y calma. </p>'+
+  '        </div>'+
+  '    </div>'+
+  '    <div class="box__footer">'+
+  '        <h2>Secciones</h2>'+
+  '        <a href="/index.html">Inicio</a>'+
+  '        <a href="../pages/quienessomos.html">¿Quienes somos?</a>'+
+  '        <a href="../pages/tienda.html">Tienda</a>'+
+  '        <a href="../pages/contacto.html">Contacto</a>'+
+  '    </div>'+
+  '    <div class="box__footer">'+
+  '        <h2>Redes Sociales</h2>'+
+  '        <a href="https://web.whatsapp.com/" target="_blank"> <i class="fab fa-whatsapp-square"></i> Whatsapp</a>'+
+  '        <a href="https://www.facebook.com/" target="_blank"> <i class="fab fa-facebook-square"></i> Facebook</a>'+
+  '        <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square"></i> Twitter</a>'+
+  '        <a href="https://www.instagram.com/huerta_zuculenta/" target="_blank"><i class="fab fa-instagram-square"></i> Instagram</a>'+
+  '    </div>'+
+  '</div>'+
+  '<div class="box__copyright">'+
+  '    <hr>'+
+  '    <p>Trabajo final BA Multiplica 2.0  © 2023 <b>Zulma Marcos</b></p>'+
+  '</div>'+
+  '</footer>';
   
   
 
